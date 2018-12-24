b92df14583ab210e9e459aca4a4c38f8823d33c9
Pairploy <pnsound123456@gmail.com>
2018-12-24 16:27:39 +0700

    update 24

---
+++ Monday24Lab5/Monday24Lab5/Form1.Designer.vb
@@ -0,0 +1,349 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.gpbPaid = New System.Windows.Forms.GroupBox()
+        Me.radCredit = New System.Windows.Forms.RadioButton()
+        Me.radPaid = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.radMember = New System.Windows.Forms.RadioButton()
+        Me.radOther = New System.Windows.Forms.RadioButton()
+        Me.txtUnit = New System.Windows.Forms.TextBox()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.txtProduct = New System.Windows.Forms.TextBox()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblDiscount = New System.Windows.Forms.Label()
+        Me.lblCredit = New System.Windows.Forms.Label()
+        Me.lblPaid = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.gpbPaid.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(317, 465)
+        Me.btnCalculate.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(94, 29)
+        Me.btnCalculate.TabIndex = 39
+        Me.btnCalculate.Text = "คำนวณ"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'gpbPaid
+        '
+        Me.gpbPaid.Controls.Add(Me.radCredit)
+        Me.gpbPaid.Controls.Add(Me.radPaid)
+        Me.gpbPaid.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.gpbPaid.Location = New System.Drawing.Point(436, 190)
+        Me.gpbPaid.Margin = New System.Windows.Forms.Padding(4)
+        Me.gpbPaid.Name = "gpbPaid"
+        Me.gpbPaid.Padding = New System.Windows.Forms.Padding(4)
+        Me.gpbPaid.Size = New System.Drawing.Size(255, 85)
+        Me.gpbPaid.TabIndex = 38
+        Me.gpbPaid.TabStop = False
+        Me.gpbPaid.Text = "การชำระเงิน"
+        '
+        'radCredit
+        '
+        Me.radCredit.AutoSize = True
+        Me.radCredit.Location = New System.Drawing.Point(172, 39)
+        Me.radCredit.Margin = New System.Windows.Forms.Padding(4)
+        Me.radCredit.Name = "radCredit"
+        Me.radCredit.Size = New System.Drawing.Size(57, 27)
+        Me.radCredit.TabIndex = 0
+        Me.radCredit.TabStop = True
+        Me.radCredit.Text = "เชื่อ"
+        Me.radCredit.UseVisualStyleBackColor = True
+        '
+        'radPaid
+        '
+        Me.radPaid.AutoSize = True
+        Me.radPaid.Location = New System.Drawing.Point(21, 39)
+        Me.radPaid.Margin = New System.Windows.Forms.Padding(4)
+        Me.radPaid.Name = "radPaid"
+        Me.radPaid.Size = New System.Drawing.Size(51, 27)
+        Me.radPaid.TabIndex = 0
+        Me.radPaid.TabStop = True
+        Me.radPaid.Text = "สด"
+        Me.radPaid.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radMember)
+        Me.GroupBox1.Controls.Add(Me.radOther)
+        Me.GroupBox1.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(112, 190)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Size = New System.Drawing.Size(264, 85)
+        Me.GroupBox1.TabIndex = 37
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "ประเภทลูกค้า"
+        '
+        'radMember
+        '
+        Me.radMember.AutoSize = True
+        Me.radMember.Location = New System.Drawing.Point(145, 39)
+        Me.radMember.Margin = New System.Windows.Forms.Padding(4)
+        Me.radMember.Name = "radMember"
+        Me.radMember.Size = New System.Drawing.Size(108, 27)
+        Me.radMember.TabIndex = 0
+        Me.radMember.TabStop = True
+        Me.radMember.Text = "ลูกค้าทั่วไป"
+        Me.radMember.UseVisualStyleBackColor = True
+        '
+        'radOther
+        '
+        Me.radOther.AutoSize = True
+        Me.radOther.Location = New System.Drawing.Point(21, 39)
+        Me.radOther.Margin = New System.Windows.Forms.Padding(4)
+        Me.radOther.Name = "radOther"
+        Me.radOther.Size = New System.Drawing.Size(80, 27)
+        Me.radOther.TabIndex = 0
+        Me.radOther.TabStop = True
+        Me.radOther.Text = "สมาชิก"
+        Me.radOther.UseVisualStyleBackColor = True
+        '
+        'txtUnit
+        '
+        Me.txtUnit.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUnit.Location = New System.Drawing.Point(331, 99)
+        Me.txtUnit.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtUnit.Name = "txtUnit"
+        Me.txtUnit.Size = New System.Drawing.Size(124, 30)
+        Me.txtUnit.TabIndex = 36
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPrice.Location = New System.Drawing.Point(331, 56)
+        Me.txtPrice.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(124, 30)
+        Me.txtPrice.TabIndex = 35
+        '
+        'txtProduct
+        '
+        Me.txtProduct.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtProduct.Location = New System.Drawing.Point(331, 15)
+        Me.txtProduct.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtProduct.Name = "txtProduct"
+        Me.txtProduct.Size = New System.Drawing.Size(124, 30)
+        Me.txtProduct.TabIndex = 34
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.lblTotal.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(331, 150)
+        Me.lblTotal.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(125, 21)
+        Me.lblTotal.TabIndex = 32
+        '
+        'lblDiscount
+        '
+        Me.lblDiscount.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.lblDiscount.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDiscount.Location = New System.Drawing.Point(327, 320)
+        Me.lblDiscount.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblDiscount.Name = "lblDiscount"
+        Me.lblDiscount.Size = New System.Drawing.Size(125, 21)
+        Me.lblDiscount.TabIndex = 31
+        '
+        'lblCredit
+        '
+        Me.lblCredit.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.lblCredit.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCredit.Location = New System.Drawing.Point(327, 410)
+        Me.lblCredit.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblCredit.Name = "lblCredit"
+        Me.lblCredit.Size = New System.Drawing.Size(125, 21)
+        Me.lblCredit.TabIndex = 22
+        '
+        'lblPaid
+        '
+        Me.lblPaid.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.lblPaid.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPaid.Location = New System.Drawing.Point(327, 363)
+        Me.lblPaid.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblPaid.Name = "lblPaid"
+        Me.lblPaid.Size = New System.Drawing.Size(125, 21)
+        Me.lblPaid.TabIndex = 29
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(109, 363)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(146, 23)
+        Me.Label9.TabIndex = 28
+        Me.Label9.Text = "จำนวนเงินต้องชำระ"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(109, 410)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(102, 23)
+        Me.Label7.TabIndex = 27
+        Me.Label7.Text = "ยอดค้างชำระ"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(109, 320)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(60, 23)
+        Me.Label6.TabIndex = 26
+        Me.Label6.Text = "ส่วนลด"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(109, 150)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(60, 23)
+        Me.Label5.TabIndex = 25
+        Me.Label5.Text = "เป็นเงิน"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(109, 105)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 23)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "จำนวนซื้อ"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(109, 63)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 23)
+        Me.Label3.TabIndex = 23
+        Me.Label3.Text = "ราคาขาย"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(109, 19)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 23)
+        Me.Label2.TabIndex = 33
+        Me.Label2.Text = "ชื่อสินค้า"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Unicode MS", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(432, -44)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(185, 23)
+        Me.Label1.TabIndex = 30
+        Me.Label1.Text = "โปรแกรมคำนวณการขาย"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(799, 528)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.gpbPaid)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtUnit)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.txtProduct)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lblDiscount)
+        Me.Controls.Add(Me.lblCredit)
+        Me.Controls.Add(Me.lblPaid)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.gpbPaid.ResumeLayout(False)
+        Me.gpbPaid.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents gpbPaid As GroupBox
+    Friend WithEvents radCredit As RadioButton
+    Friend WithEvents radPaid As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents radMember As RadioButton
+    Friend WithEvents radOther As RadioButton
+    Friend WithEvents txtUnit As TextBox
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents txtProduct As TextBox
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lblDiscount As Label
+    Friend WithEvents lblCredit As Label
+    Friend WithEvents lblPaid As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Monday24Lab5/Monday24Lab5/Form1.vb
@@ -0,0 +1,84 @@
+﻿Public Class Form1
+    Dim decTotal As Decimal
+    Dim decDiscount As Decimal
+    Dim decNet As Decimal
+    Dim decCredit As Decimal
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        radCredit.Checked = False
+        radMember.Checked = False
+        radOther.Checked = False
+        radPaid.Checked = False
+    End Sub
+
+    Private Sub txtPrice_TextChanged(sender As Object, e As EventArgs) Handles txtPrice.TextChanged
+        decTotal = Val(txtPrice.Text) * Val(txtUnit.Text)
+        lblTotal.Text = FormatNumber(decTotal)
+
+    End Sub
+
+    Private Sub txtUnit_TextChanged(sender As Object, e As EventArgs) Handles txtUnit.TextChanged
+
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        decTotal = FormatNumber(txtPrice.Text) * FormatNumber(txtUnit.Text)
+        lblTotal.Text = FormatNumber(decTotal)
+        If txtProduct.Text = "" Or txtPrice.Text = "" Or txtUnit.Text = "" Or Val(txtPrice.Text) < 1 Or Val(txtUnit.Text) < 1 Then
+            MessageBox.Show("ป้อนข้อมูลไม่สมบูรณ์")
+            Exit Sub
+        End If
+        If radOther.Checked = True Then
+            If radPaid.Checked = True Then
+                If decTotal <= 999 Then
+                    decDiscount = 0
+                ElseIf decTotal <= 4999 Then
+                    decDiscount = decTotal * 0.05
+                ElseIf decTotal <= 9999 Then
+                    decDiscount = decTotal * 0.1
+                ElseIf decTotal >= 10000 Then
+                    decDiscount = decTotal * 0.15
+                End If
+                lblDiscount.Text = FormatNumber(decDiscount)
+                decNet = FormatNumber(decTotal) - FormatNumber(decDiscount)
+                lblPaid.Text = FormatNumber(decNet)
+                lblCredit.Text = 0
+
+
+            ElseIf radCredit.Checked = True Then
+
+                lblDiscount.Text = 0
+                decCredit = FormatNumber(decTotal)
+                lblCredit.Text = FormatNumber(decCredit)
+                lblPaid.Text = FormatNumber(decCredit)
+
+            End If
+        End If
+        If radMember.Checked = True Then
+            lblDiscount.Text = 0
+            lblCredit.Text = 0
+            lblPaid.Text = FormatNumber(decTotal)
+
+        End If
+    End Sub
+
+
+
+    Private Sub radOther_CheckedChanged(sender As Object, e As EventArgs) Handles radOther.CheckedChanged
+        radCredit.Enabled = False
+        radCredit.Checked = False
+        radPaid.Checked = True
+        lblDiscount.Text = ""
+        lblPaid.Text = ""
+        lblCredit.Text = ""
+    End Sub
+
+    Private Sub radMember_CheckedChanged(sender As Object, e As EventArgs) Handles radMember.CheckedChanged
+
+        radCredit.Enabled = True
+        lblDiscount.Text = ""
+        lblPaid.Text = ""
+        lblCredit.Text = ""
+    End Sub
+End Class
